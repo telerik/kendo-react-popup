d835a28b1d88b230b5f2121317f842b511bb12d1
Petyo Ivanov <underlog@gmail.com>
2016-03-29 18:12:53 +0300

    feat: demo subdirs in src

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 
 import util from './util';
+import subUtil from './sub/util';
 
 import styles from '@telerik/kendo-theme-default/styles/example/main';
 
 export default function Component(props) {
     return (
-        <div {...props} className={styles.example}>Kendo UI react component{util()} </div>
+        <div {...props} className={styles.example}>Kendo UI react component{util()} {subUtil()}</div>
     );
 }
