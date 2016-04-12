9a58ceee9a5e224033226fe1b0614286bc252177
Georgi Krustev <ggkrustev@yahoo.com>
2016-04-12 16:24:45 +0300

    refactor(Popup): Rename base component to Popup

--- e2e/test.jsx
+++ e2e/test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Component from '@telerik/kendo-react-component-base';
+import Popup from '@telerik/kendo-react-component-base';
 
 // e2e-utils is a module exposed from react-tasks
 // it exports `$` and `withRoot` - higher order function for describe (example below)
@@ -9,9 +9,9 @@ import { withRoot } from 'e2e-utils';
 // `root` parameter is a jQuery object which includes chai-jquery in it.
 // chai-jquery adds a should method to the jQuery object.
 // See https://github.com/chaijs/chai-jquery#assertions for available assertions.
-describe('Component', withRoot(root => {
+describe('Popup', withRoot(root => {
     it('should render a div', () => {
-        ReactDOM.render( <Component />, root[0]);
+        ReactDOM.render( <Popup />, root[0]);
         root.find("div").should.have.class("k-example");
     });
 }));
--- test/test.jsx
+++ test/test.jsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
-import Component from '../src/Component';
+import Popup from '../src/Popup';
 
-describe('Component', () => {
+describe('Popup', () => {
     let result;
 
     beforeEach(() => { /* test setup */ });
 
     it('should render a div', () => {
-        result = shallow(<Component />);
+        result = shallow(<Popup />);
         expect(result.type()).toEqual('div');
     });
 });
